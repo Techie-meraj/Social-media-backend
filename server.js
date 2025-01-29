3e11f2e4bf4aa4c528d241bb9987f98be4e6aef0
MERAJ AHMAD <154743674+Techie-meraj@users.noreply.github.com>
2025-01-30 00:12:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,12 @@ const PORT=process.env.PORT
 const app=express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: 'http://localhost:5173' }));
+
+app.use(cors({
+  origin: "https://social-media-frontend-fawn.vercel.app", // Your deployed frontend
+  credentials: true
+}));
+
 
 app.use('/user',userRouter)
 app.use('/post',postrouter)
